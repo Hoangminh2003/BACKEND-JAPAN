9ff0cb4aa175fe8ff6797330c107bd20ea90639e
NguyenPhuocYen <nguyenphuocyen11@gmail.com>
2026-05-01 15:40:13 +0700

    chore(base-repo): update base repo

---
+++ src/repositories/base.repository.js
@@ -0,0 +1,18 @@
+/**
+ * Base repository with common CRUD operations.
+ * All repositories extend this class.
+ */
+export default class BaseRepository {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async findById(id, options = {}) {
+        const query = this.model.findById(id);
+        if (options.select) query.select(options.select);
+        if (options.populate) query.populate(options.populate);
+        if (options.lean) query.lean();
+        return query;
+    }
+
+}
