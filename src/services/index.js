884cfdce8af199f7153acf79eca46266ad1dd256
NguyenPhuocYen <nguyenphuocyen11@gmail.com>
2026-05-09 13:25:01 +0700

    chore(index):add index

---
+++ src/services/index.js
@@ -0,0 +1,8 @@
+export { default as adminService } from "./admin.service.js";
+export { default as aiService } from "./ai.service.js";
+export { default as authService } from "./auth.service.js";
+export { default as examAttemptService } from "./exam-attempt.service.js";
+export { default as examFeedbackService } from "./exam-feedback.service.js";
+export { default as examService } from "./exam.service.js";
+export { default as questionBlockService } from "./question-block.service.js";
+export { default as uploadService } from "./upload.service.js";
